f131ca1ee35ab3ea489950b18e0a82be97009021
abdurraihan565 <128666297+abdurraihan565@users.noreply.github.com>
2024-06-28 10:44:38 +0600

    Update secret.js

--- src/secret.js
+++ src/secret.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const serverPort = https://clickads.onrender.com/ || 3003;
+const serverPort = process.env.SERVER_PORT || 3003;
 const MongodbURL =
   process.env.MONGODB_ATLAS_URL || 'mongodb://localhost:27017/clickAds';
 const defaultImagePath =

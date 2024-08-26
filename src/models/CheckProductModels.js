e3c096ef3a1ed18ae8b5ac42d403a487f8c33456
Abdur Rayhan <128666297+abdurraihan565@users.noreply.github.com>
2024-08-26 21:03:00 +0600

    Update CheckProductModels.js

--- src/models/CheckProductModels.js
+++ src/models/CheckProductModels.js
@@ -1,1 +1,26 @@
+const { Schema, model } = require('mongoose');
 
+const checkProductSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'email is Required'],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validator: function (v) {
+        return /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/.test(v);
+      },
+      message: 'please inter a valid email',
+    },
+    productid: {
+      type: String,
+      required: [true, 'productid  is Required'],
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const checkProduct = model('checkProduct', checkProductSchema);
+module.exports = checkProduct;
